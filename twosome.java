6d1aff1cead89d20c94ef9d469e11c1dfa87da16
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2019-12-10 18:51:34 -0500

    Update #2

--- twosome.java
+++ twosome.java
@@ -7,8 +7,7 @@ class Solution {
         for(int i = 0; i <= len; i++){
             for(int j = i; j <= len; j++){
                 if(nums[i] + nums[j] == target){
-                    int[] ans = {i, j};
-                    return ans;
+                    return new int[] = {i, j};
                 }
             }
         }

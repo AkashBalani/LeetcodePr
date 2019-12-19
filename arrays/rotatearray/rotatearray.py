07211c56663548cfe0fd08a84e8b030655f09eb5
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2019-12-18 20:41:33 -0500

    Added Comments

--- arrays/rotatearray/rotatearray.py
+++ arrays/rotatearray/rotatearray.py
@@ -1,10 +1,10 @@
 ### Python Accepted Solution ###
 
 class Solution(object):
-    def rotate(self, nums, k):                    
-        if k is None or k <= 0:
+    def rotate(self, nums, k):                                  #self here is the instance of the class itself                     
+        if k is None or k <= 0:                                 #Edge case
             return
-        k, end = k % len(nums), len(nums) - 1
+        k, end = k % len(nums), len(nums) - 1                   #Multivariable assignment
         self.reverse(nums, 0, end - k)
         self.reverse(nums, end - k + 1, end)
         self.reverse(nums, 0, end)

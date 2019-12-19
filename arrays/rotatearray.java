c494ab2609319f40d6ce64de869fef854cb9767e
AkashBalani <44540729+AkashBalani@users.noreply.github.com>
2019-12-18 19:51:04 -0500

    Fix

--- arrays/rotatearray.java
+++ arrays/rotatearray.java
@@ -1,26 +1,16 @@
 //Brute Force
-//initial Attempt
+//Accepted Answer
 
-class Solution {
+public class Solution {
     public void rotate(int[] nums, int k) {
-        int[] ans = new int[nums.length];
-        int marker = k;
-        if(nums.length == 1 || k < 1){
-            return;
-        }
-        for(int i = 0; i <= k && marker <= nums.length; i++){
-            //ans[i] = 0;
-            ans[marker] = nums[i];
-            marker++;
-        }
-        marker = k+1;
-        for(int i = 0; i < k; i++){
-            ans[i] = nums[marker];
-            marker++;
-        }
-        System.out.println(Arrays.toString(ans));
-        for(int i = 0; i < nums.length; i++){
-            nums[i] = ans[i];
+        int temp, previous;
+        for (int i = 0; i < k; i++) {
+            previous = nums[nums.length - 1];
+            for (int j = 0; j < nums.length; j++) {
+                temp = nums[j];
+                nums[j] = previous;
+                previous = temp;
+            }
         }
     }
 }

13b4923664579a3b6c485fbb081da62525c8ae32
AkashBalani <balani.a@husky.neu.edu>
2019-12-22 20:27:22 -0500

    Documentation Changes

--- arrays/rotatearray/rotatearray.java
+++ arrays/rotatearray/rotatearray.java
@@ -2,7 +2,7 @@
 //Accepted Answer
 
 public class Solution {
-    public void rotate(int[] nums, int k) {
+    public void rotate(int[] nums, int k) {           //Example nums = [1, 2, 3, 4, 5, 6, 7], k = 4
         int temp, previous;
         for (int i = 0; i < k; i++) {
             previous = nums[nums.length - 1];

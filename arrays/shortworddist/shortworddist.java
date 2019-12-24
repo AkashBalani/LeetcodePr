5d3d25a35a918ffbe06ff0f9f779adfcc9771bbb
AkashBalani <balani.a@husky.neu.edu>
2019-12-23 20:37:32 -0500

    Second Brute force attempt

--- arrays/shortworddist/shortworddist.java
+++ arrays/shortworddist/shortworddist.java
@@ -3,7 +3,7 @@
 
 class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
-        int alpha = 0;
+        int alpha = -1;
         int beta = 0;
         for(int i = 0; i < words.length; i++){
             if(words[i].equals(word1)){
@@ -11,6 +11,8 @@ class Solution {
             }
             if(words[i].equals(word2)){
                 beta = i;
+                if(alpha != -1)
+                    return Math.abs(alpha - beta);
             }
         }
         return Math.abs(alpha - beta);

6b8a1f9cda8d943826790b1d22a4a63092b1983f
AkashBalani <balani.a@husky.neu.edu>
2019-12-25 18:19:50 -0500

    Added Python solution for the same problem

---
+++ arrays/buyAndSellStock2/buyAndSellStock2.py
@@ -0,0 +1,5 @@
+## The code pretty self explanitory
+
+class Solution(object):
+    def maxProfit(self, prices):
+        return sum(max(prices[i + 1] - prices[i], 0) for i in range(len(prices) - 1))

3ca44903070c21146667d54337c280921a73c680
AkashBalani <balani.a@husky.neu.edu>
2019-12-25 18:15:09 -0500

    Added Problem based on Stocks

---
+++ arrays/buyAndSellStock2/buyAndSellStock2.java
@@ -0,0 +1,13 @@
+// Accepted Answer
+// In this problem this like you are standing in front of the array, literally
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int total = 0;
+        for (int i=0; i< prices.length-1; i++) {
+            if (prices[i+1]>prices[i]) total += prices[i+1]-prices[i];
+        }
+        
+        return total;
+    }
+}

03aeefb90be72a9bb059935495c47bd85d6eea23
AkashBalani <balani.a@husky.neu.edu>
2019-12-25 17:25:48 -0500

    Documentation changes

--- arrays/removeDuplicates/removeDuplicates.java
+++ arrays/removeDuplicates/removeDuplicates.java
@@ -2,8 +2,8 @@
 
 class Solution {
     public int removeDuplicates(int[] nums) {
-    int i = nums.length > 0 ? 1 : 0; 
-    for (int n : nums)
+    int i = nums.length > 0 ? 1 : 0;  // This one makes sure that if nums.length == 0 we dont process it
+    for (int n : nums) // Enhanced for loop
         if (n > nums[i-1])
             nums[i++] = n;
     return i;

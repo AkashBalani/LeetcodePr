49040359818ab47c288b09b84c8965ae2f266a14
AkashBalani <balani.a@husky.neu.edu>
2019-12-26 13:24:20 -0500

    Added a solution with better performance

--- arrays/containsDuplicates/containsDuplicates.java
+++ arrays/containsDuplicates/containsDuplicates.java
@@ -1,15 +1,16 @@
 // Brute Force
-// Accepted Answer somehow?
-// Space --> O(1) and Time --> O(n*logn)
-// This is not including the sort, shame
+// Accepted Answer Using Set
+// Space --> O(n) and Time --> O(n)
+// Weird But beautiful
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int ind = 1; ind < nums.length; ind++) {
-            if(nums[ind] == nums[ind - 1]) {
+        final Set<Integer> distinct = new HashSet<Integer>();
+        for(int num : nums) {
+            if(distinct.contains(num)) {
                 return true;
             }
+            distinct.add(num);
         }
         return false;
     }

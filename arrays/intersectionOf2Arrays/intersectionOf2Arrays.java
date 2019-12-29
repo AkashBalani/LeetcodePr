c0130a93af9f9d4e101a6cf67ee59c927c9184be
AkashBalani <balani.a@husky.neu.edu>
2019-12-28 19:51:51 -0500

    Added the solution

--- arrays/intersectionOf2Arrays/intersectionOf2Arrays.java
+++ arrays/intersectionOf2Arrays/intersectionOf2Arrays.java
@@ -1,19 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 // Initial Solution
 // Passed some testcases
 
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int[] res = new int[Math.min(nums1.length, nums2.length)];
-        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
         for(int i = 0; i < nums1.length; i++){
-            for(int j = 0; j < nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    res[count] = nums1[i];
-                    nums2[j] = Integer.MAX_VALUE;
-                    count++;
-                }    
+            if(map.containsKey(nums1[i])) map.put(nums1[i], nums1[i]+1);
+            else map.put(nums1[i], 1);
+        }
+        for(int i = 0; i < nums2.length; i++){
+            if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                result.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]-1));
             }
         }
-        return res;
+
+        int[] r = new int[result.size()];
+        for(int i = 0; i < result.size(); i++){
+            r[i] = result.get(i);
+        }
     }
 }

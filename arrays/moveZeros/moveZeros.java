45c805fa0bd37c563879f37a9b2493c05dcc128b
AkashBalani <balani.a@husky.neu.edu>
2020-01-02 01:36:49 -0500

    Move Zeros Cheat Solution

--- arrays/moveZeros/moveZeros.java
+++ arrays/moveZeros/moveZeros.java
@@ -11,4 +11,17 @@ class Solution {
           nums[insertPos++] = 0;
         }
     }
+
+    public void moveZeroes1(int[] nums) {
+
+      int j = 0;
+      for(int i = 0; i < nums.length; i++) {
+          if(nums[i] != 0) {
+              int temp = nums[j];
+              nums[j] = nums[i];
+              nums[i] = temp;
+              j++;
+            }
+      }
+    }
 }

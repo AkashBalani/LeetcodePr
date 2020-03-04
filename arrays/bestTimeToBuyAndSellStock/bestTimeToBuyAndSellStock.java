2d804349404913c41834f2ea18276c444c75f412
AkashBalani <balani.a@husky.neu.edu>
2020-03-03 20:19:39 -0500

    Added a solution with Kadane's Algorithm

--- arrays/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.java
+++ arrays/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.java
@@ -64,3 +64,20 @@ public class Solution {
         return maxprofit;
     }
 }
+
+// Accpeted Solution
+// Based on the Kadane's Algorithm
+// Can be used for Negative numbers
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int maxCur = 0, maxSoFar = 0;
+        for(int i = 1; i < prices.length; i++) {
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = Math.max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
+}
+
+

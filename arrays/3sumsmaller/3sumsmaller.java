94f1c3427657de05ae785ec4f6fe9ffbdfc02231
AkashBalani <balani.a@husky.neu.edu>
2020-03-04 14:02:22 -0500

    Added Problem Discription for 3SumSmaller

--- arrays/3sumsmaller/3sumsmaller.java
+++ arrays/3sumsmaller/3sumsmaller.java
@@ -1,3 +1,8 @@
+// Given an array of "n" integers "nums" and a "target", find the number of index triplets "i, j, k" with "0 <= i < j < k < n" that satisfy the condition "nums[i] + nums[j] + nums[k] < target"
+
+// INPUT: nums = [-2, 0, 1, 3], and target = 2
+// OUTPUT: 2
+// EXPLANATION: [-2,0,1] and [-2,0,3]
 // Accepted Solution
 
 public class Solution {

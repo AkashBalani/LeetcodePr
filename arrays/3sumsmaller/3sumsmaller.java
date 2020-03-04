bf81872406fa1019fcc29d74cfe25ffce4a96ad9
AkashBalani <balani.a@husky.neu.edu>
2020-03-04 16:02:34 -0500

    Added comments to 3SumSmaller

--- arrays/3sumsmaller/3sumsmaller.java
+++ arrays/3sumsmaller/3sumsmaller.java
@@ -10,14 +10,18 @@ public class Solution {
     
     public int threeSumSmaller(int[] nums, int target) {
         count = 0;
-        Arrays.sort(nums);
+        Arrays.sort(nums); 
+        // This sorting should take O(nlogn)
         int len = nums.length;
     
         for(int i=0; i<len-2; i++) {
+            // For loop takes n iterations
             int left = i+1, right = len-1;
             while(left < right) {
+                // This while loop would make the complexity O(n2)
                 if(nums[i] + nums[left] + nums[right] < target) {
                     count += right-left;
+                    // This line is the main reason this code is so small, we are essentially saying that if we have found such a condition where i, left and right added up are less than target than any elements in between would also satisfy this condition as the array is sorted             
                     System.out.println(count);
                     left++;
                 } else {

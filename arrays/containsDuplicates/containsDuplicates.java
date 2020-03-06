a813a26f0345f96a532845c6def7de298c84d88b
AkashBalani <balani.a@husky.neu.edu>
2020-03-05 20:24:07 -0500

    Added Description for containsDuplicates problem

--- arrays/containsDuplicates/containsDuplicates.java
+++ arrays/containsDuplicates/containsDuplicates.java
@@ -1,4 +1,8 @@
-// Brute Force
+// Given an array of integers find if it contains any duplicates
+
+// INPUT: [1,2,3,1]
+// OUTPUT: true
+
 // Accepted Answer Using Set
 // Space --> O(n) and Time --> O(n)
 // Weird But beautiful

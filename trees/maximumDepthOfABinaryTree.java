f6f5f81c332805556c0bc51b6a89fdcbe16d32bb
AkashBalani <balani.a@husky.neu.edu>
2020-03-21 16:01:56 -0400

    Added Iterative solution to MaxDepth

--- trees/maximumDepthOfABinaryTree.java
+++ trees/maximumDepthOfABinaryTree.java
@@ -1,14 +1,18 @@
+import java.util.LinkedList;
+
+import javax.swing.tree.TreeNode;
+
 // Accepted Solution
 // Uses DFS approach
 
-public class TreeNode{
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x){
-        val = x;
-    }
-}
+// public class TreeNode{
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+//     TreeNode(int x){
+//         val = x;
+//     }
+// }
 class Solution{
     public int maxDepth(TreeNode root){
         if(root == null){
@@ -17,5 +21,32 @@ class Solution{
         int left_height = maxDepth(root.left);
         int right_height = maxDepth(root.right);
         return java.lang.Math.max(left_height, right_height) + 1;
+    }
+}
+
+// Accepted Solution
+// DFS + Iterative
+class Solution1{
+    public int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> depths = new LinkedList<>();
+        int depth = 0, current_depth = 0;
+        stack.add(root);
+        depths.add(1);
+        while(!stack.isEmpty()){
+            root = stack.pollLast();
+            current_depth = depths.pollLast();
+            if(root != null){
+                depth = Math.max(depth, current_depth);
+                stack.add(root.left);
+                depths.add(current_depth + 1);
+                stack.add(root.right);
+                depths.add(current_depth + 1);
+            }
+        }
+        return depth;
     }
 }

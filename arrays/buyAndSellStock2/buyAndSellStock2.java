d28bcb073972e9f83bfc20df5fb98eea47c6596e
AkashBalani <balani.a@husky.neu.edu>
2020-03-24 18:00:44 -0400

    Added another solution for buyAndSellStock2

--- arrays/buyAndSellStock2/buyAndSellStock2.java
+++ arrays/buyAndSellStock2/buyAndSellStock2.java
@@ -1,7 +1,7 @@
 // Accepted Answer
 // In this problem this like you are standing in front of the array, literally
 
-public class Solution {
+class Solution {
     public int maxProfit(int[] prices) {
         int total = 0;
         for (int i=0; i< prices.length-1; i++) {
@@ -9,5 +9,24 @@ public class Solution {
         }
         
         return total;
+    }
+}
+
+class Solution1 {
+    public int maxProfit(int[] prices){
+        int peak = prices[0];
+        int valley = prices[0];
+        int maxProfit = 0;
+        int i = 0;
+        while(i < prices.length - 1){
+            while(i < prices.length - 1 && prices[i] >= prices[i+1])
+                i++;
+            valley = prices[i];
+            while(i < prices.length - 1 && prices[i] <= prices[i+1])
+                i++;
+            peak = prices[i];
+            maxProfit += peak - valley;
+        }
+        return maxProfit;
     }
 }

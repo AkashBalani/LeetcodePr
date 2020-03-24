b7ffc2d3a30e325343ffa3c119d553917e3503c7
AkashBalani <balani.a@husky.neu.edu>
2020-03-24 17:28:44 -0400

    Added optimum solution for mergeSortedArrays

--- sortingAndSearching/mergeSortedArrays.java
+++ sortingAndSearching/mergeSortedArrays.java
@@ -8,6 +8,8 @@ class Solution{
     }
 }
 
+// Time Complexity O(m + n)
+// Space Complexity O(m)
 class Solution1{
     public void merge(int[] nums1, int m, int[] nums2, int n){
         int[] nums1_copy = new int[m];
@@ -22,5 +24,16 @@ class Solution1{
             System.arraycopy(nums1_copy, p1, nums1, p, m + n - p);
         if(p2 < n)
             System.arraycopy(nums2, p2, nums1, p, m + n - p);
+    }
+}
+
+class Solution2{
+    public void merge(int[] nums1, int m, int[] nums2, int n){
+        int p1 = m - 1, p2 = n - 1, p = nums1.length - 1;
+        
+        while( (p1 >= 0) && (p2 >= 0) )
+            nums1[p--] = (nums1[p1] > nums2[p2]) ? nums1[p1--] : nums2[p2--];
+
+        System.arraycopy(nums2, 0, nums1, 0, p2+1);
     }
 }

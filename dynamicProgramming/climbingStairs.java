2c304d29f398b0dab50da357f3e219c5cfbd8d99
AkashBalani <balani.a@husky.neu.edu>
2020-03-25 16:19:50 -0400

    Added Dynamic Programming

---
+++ dynamicProgramming/climbingStairs.java
@@ -0,0 +1,9 @@
+// Initial Attempt
+// Time Limit Exceeded
+class Solution{
+    public int climbStairs(int n){
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        else return climbStairs(n-1) + climbStairs(n-2);
+    }
+}

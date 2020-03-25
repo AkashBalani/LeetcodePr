70d72d8bf29d0b5bc85db288f1d3b7f646932499
AkashBalani <balani.a@husky.neu.edu>
2020-03-25 16:51:05 -0400

    Added Fibonaci Solution

--- dynamicProgramming/climbingStairs.java
+++ dynamicProgramming/climbingStairs.java
@@ -22,3 +22,19 @@ class Solution1{
         return dp[n];
     }
 }
+
+// Uses Fibonaci Sequence
+// The difference between this one and the one before it is space complexity
+class Solution2{
+    public int climbStairs(int n){
+        if(n == 1) return 1;
+        int first = 1;
+        int second = 2;
+        for(int i = 3; i <= n; i++){
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}

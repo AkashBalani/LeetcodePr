f259e8f8da4a25cce313941d17c594d324fbf8ce
AkashBalani <balani.a@husky.neu.edu>
2020-04-07 21:10:18 -0400

    Added files for merginf two lists

---
+++ linkedList/mergeTwoLists.java
@@ -0,0 +1,22 @@
+class Solution{
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){ 
+            val = x;
+        }
+    }
+    // Recursive Solution
+    public ListNode mergeTwoList(ListNode l1, ListNode l2){
+        if(l1 == null) return l2;
+        else if(l2 == null) return l1;
+        else if(l1.val < l2.val){
+            l1.next = mergeTwoList(l1.next, l2);
+            return l1;
+        }
+        else{
+            l2.next = mergeTwoList(l1, l2.next);
+            return l2;
+        }
+    }
+}

981228095517b8ebc0efd4ccffcadfabc695c369
AkashBalani <balani.a@husky.neu.edu>
2020-04-12 18:03:53 -0400

    Added another approach to solve lastStoneWeight

--- arrays/lastStoneWeight.java
+++ arrays/lastStoneWeight.java
@@ -23,3 +23,27 @@ class Solution4{
         return result;
     }
 }
+
+class Solution5{
+    public int lastStoneWeight(int[] stones){
+        List<Integer> stoneList = new ArrayList<>();
+        for(int x : stones)
+            stoneList.add(x);
+        Collections.sort(stoneList);
+        while(stoneList.size() > 1){
+            int stone1 = stoneList.remove(stoneList.size() - 1);
+            int stone2 = stoneList.remove(stoneList.size() - 1);
+            if(stone1 != stone2){
+                int newStone = stone1 - stone2;
+                int index = Collections.binarySearch(stoneList, newStone);
+                if(index < 0){
+                    stoneList.add(- index - 1, newStone);
+                }
+                else{
+                    stoneList.add(index, newStone);
+                }
+            }
+        }
+        return !stoneList.isEmpty() ? stoneList.remove(0) : 0;
+    }
+}

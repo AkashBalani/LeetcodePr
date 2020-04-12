7561ff403c3c080fb76b52c847cbb5badf36522c
AkashBalani <balani.a@husky.neu.edu>
2020-04-12 18:09:23 -0400

    Added heap solution for lastStoneWeight

--- arrays/lastStoneWeight.java
+++ arrays/lastStoneWeight.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 class Solution4{
     public int lastStoneWeight(int[] stones){
@@ -45,5 +48,22 @@ class Solution5{
             }
         }
         return !stoneList.isEmpty() ? stoneList.remove(0) : 0;
+    }
+}
+
+class Solution6{
+    public int lastStoneWeight(int[] stones){
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int x : stones)
+            heap.add(x);
+        
+        while(heap.size() > 1){
+            int stone1 = heap.remove();
+            int stone2 = heap.remove();
+            if(stone1 != stone2){
+                heap.add(stone1 - stone2);
+            }
+        }
+        return heap.isEmpty() ? 0 : heap.remove();
     }
 }

621120920fb6fb3c674182246a77c96c75cab86b
AkashBalani <balani.a@husky.neu.edu>
2020-04-13 17:06:20 -0400

    Removed unused imports

--- arrays/lastStoneWeight.java
+++ arrays/lastStoneWeight.java
@@ -3,7 +3,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 class Solution4{
     public int lastStoneWeight(int[] stones){

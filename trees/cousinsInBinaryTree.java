2375482282ee3dccd33fc8164bd779aaa9a519dd
AkashBalani <balani.a@husky.neu.edu>
2020-05-07 16:24:26 -0400

    Added BFS Solution to Binary Tree Problem

--- trees/cousinsInBinaryTree.java
+++ trees/cousinsInBinaryTree.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
 
 // DFS Solution
 class Solution{
@@ -25,7 +29,42 @@ class Solution{
     }
 
     public boolean isCousins(TreeNode root,int x,int y){
-        dfs(root, x, y);
+        dfs(root, 0, x, y);
         return this.cousins;
+    }
+}
+
+// BFS Solution
+class Solution1{
+    public boolean isCousins(TreeNode root, int x, int y){
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            boolean siblings = false;
+            boolean cousins = false;
+
+            int nodesAtDepth = queue.size();
+            for(int i = 0; i < nodesAtDepth; i++){
+                TreeNode node = queue.remove();
+
+                if(node == null) siblings = false;
+                else{
+                    if(node.val == x || node.val == y){
+                        if(!cousins)
+                            siblings = cousins = true;
+                        else
+                            return !siblings;
+                    }
+
+                    if(node.left != null) queue.add(node.left);
+                    if(node.right != null) queue.add(node.right);
+                    queue.add(null);
+                }
+            }
+
+            if(cousins) return false;
+        }
+        return false;
     }
 }

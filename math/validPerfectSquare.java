b43886a13e9b65893f69bee389eaf1c6c2a616b5
AkashBalani <balani.a@husky.neu.edu>
2020-05-09 18:07:48 -0400

    Added Newton's method solution for perfect square

--- math/validPerfectSquare.java
+++ math/validPerfectSquare.java
@@ -17,3 +17,14 @@ class Solution{
         return false;
     }
 }
+
+class Solution1 {
+    public boolean isValidSquare(int num){
+        if(num < 2) return true;
+
+        long x = num / 2;
+        while(x * x > num)
+            x = (x + num / x) / 2;
+    }
+    return (x * x == num);
+}

025e84507a0d2d785b52d44b6861e228c7079e0e
AkashBalani <balani.a@husky.neu.edu>
2020-05-10 14:31:47 -0400

    Added single array approach to Find Judge

--- graphs/findTownJudge.java
+++ graphs/findTownJudge.java
@@ -41,3 +41,20 @@ class Solution1{
         return -1;
     }
 }
+
+// Single Array Approach
+class Solution2 {
+    public int findJudge(int[][] trust){
+        if(trust.length < N - 1) return -1;
+        int[] score = new int[N + 1];
+        for(int[] relation : trust){
+            score[relation[1]]++;
+            score[relation[0]]--;
+        }
+        for(int i = 0; i <= N; i++){
+            if(score[i] == N-1)
+                return i;
+        }
+        return -1;
+    }
+}

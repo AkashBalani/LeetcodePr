7ff10f8a2a1ade6c9d08fdc02e42a15d0a6f25a7
AkashBalani <balani.a@husky.neu.edu>
2020-05-10 17:43:50 -0400

    Added Solution for Valid Palindrome problem

--- strings/validPalindrome.java
+++ strings/validPalindrome.java
@@ -14,3 +14,21 @@ class Solution{
         return s1.equals(s2);
     }
 }
+
+// Two Pointer Solution
+class Solution1{
+    public boolean isValidPalindrome(String s){
+        for(int i = 0, j = s.length() - 1; i < j; i++, j--){
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+            }
+            while(i < j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+            while(i < j && Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+}

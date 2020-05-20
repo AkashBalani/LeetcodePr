a5e04f1b005b7c82d185d9cc4b36dbf16125043a
AkashBalani <balani.a@husky.neu.edu>
2020-05-20 13:42:30 -0400

    Added Iterative Solution for Kth Smallest Node

--- trees/kthSmallestNode.java
+++ trees/kthSmallestNode.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+
+import javax.swing.tree.TreeNode;
 
 class Solution{
     public int kthSmallest(TreeNode root, int k){
@@ -12,5 +15,22 @@ class Solution{
         arr.add(root.val);
         inorder(root.right, arr);
         return arr;
+    }
+}
+
+// Iterative Solution
+class Solution1{
+    public int kthSmallest(TreeNode root, int k){
+        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+
+        while(true){
+            while(root != null){
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.removeLast();
+            if(--k == 0) return root.val;
+            root = root.right;
+        }
     }
 }

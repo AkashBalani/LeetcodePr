22471ddfa6c6cb9ad38a17326c4c58b2c17f5f14
AkashBalani <balani.a@husky.neu.edu>
2020-05-22 17:32:33 -0400

    Added an easier solution to the previous problem

--- arrays/sortCharactersByFrequency.java
+++ arrays/sortCharactersByFrequency.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -24,5 +25,29 @@ class Solution{
             }
         }
         return sb.toString();
+    }
+}
+
+class Solution1{
+    public String countFrequency(String s){
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        sb.append(chars[i]);
+        for(int i = 1; i < chars.length; i++){
+            if(chars[i] != chars[i - 1]){
+                list.add(sb.toString());
+                sb = new StringBuilder();
+            }
+            sb.append(chars[i]);
+        }
+
+        Collections.sort(list, (a, b) -> b.length() - a.length());
+
+        StringBuilder result = new StringBuilder();
+        for(String str : list)result.append(str);
+        return result.toString();
     }
 }

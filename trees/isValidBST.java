9a50da7d3ff509eb408361e37c574a2e33640b60
AkashBalani <balani.a@husky.neu.edu>
2020-05-23 15:20:30 -0400

    Added inorder solution for BST

--- trees/isValidBST.java
+++ trees/isValidBST.java
@@ -46,3 +46,25 @@ class Solution {
       return true;
     }
   }
+
+  class Solution2 {
+    public boolean isValidBST(TreeNode root) {
+      Stack<TreeNode> stack = new Stack();
+      double inorder = - Double.MAX_VALUE;
+  
+      while (!stack.isEmpty() || root != null) {
+        while (root != null) {
+          stack.push(root);
+          root = root.left;
+        }
+        root = stack.pop();
+        // If next element in inorder traversal
+        // is smaller than the previous one
+        // that's not BST.
+        if (root.val <= inorder) return false;
+        inorder = root.val;
+        root = root.right;
+      }
+      return true;
+    }
+  }

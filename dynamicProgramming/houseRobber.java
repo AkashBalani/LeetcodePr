8ce469e400aa71015bba90270467dff0ee174d56
AkashBalani <balani.a@husky.neu.edu>
2020-05-24 22:31:29 -0400

    Added working solution for houseRobber

--- dynamicProgramming/houseRobber.java
+++ dynamicProgramming/houseRobber.java
@@ -9,3 +9,16 @@ class Solution {
         return evenSum > oddSum ? evenSum : oddSum;
     }
 }
+
+class Solution1{
+    public int rob(int[] nums){
+        int prevMax = 0;
+        int currMax = 0;
+        for(int x : nums){
+            int temp = currMax;
+            currMax = Math.max(prevMax + x, currMax);
+            prevMax = temp;
+        }
+        return currMax;
+    }
+}

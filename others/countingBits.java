d414573403fb848627f1f60e2be3c86a26a99409
AkashBalani <balani.a@husky.neu.edu>
2020-05-28 14:56:14 -0400

    Added code for counting Bits

---
+++ others/countingBits.java
@@ -0,0 +1,17 @@
+class Solution{
+    public int[] countingBits(int num){
+        int[] ans = new int[num + 1];
+        for(int i = 0; i <= num; i++){
+            ans[i] = popcount(i);
+        }
+        return ans;
+    }
+
+    private int popcount(int x){
+        int count;
+        for(count = 0; x != 0; ++count){
+            x &= (x-1);
+        }
+        return count;
+    }
+}

26fa4e42349fc285b39659f2a021bd2c86f4b527
AkashBalani <balani.a@husky.neu.edu>
2020-05-28 15:26:07 -0400

    Added another solution for counting bits

--- others/countingBits.java
+++ others/countingBits.java
@@ -15,3 +15,19 @@ class Solution{
         return count;
     }
 }
+
+class Solution1{
+    public int countingBits(int num){
+        int[] ans = new int[num + 1];
+        int i = 0, b = 1;
+        while(b <= num){
+            while(i < b && (b + i) <= num){
+                ans[i + b] = ans[i] + 1;
+                ++i;
+            }
+            i = 0;
+            b <<= 1;
+        }
+        return ans;
+    }
+}

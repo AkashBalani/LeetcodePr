0d1c5a03de0538376b5b3d98dea6d899191dd9bf
AkashBalani <balani.a@husky.neu.edu>
2020-05-30 19:09:53 -0400

    Added a much fancier solution to KClosestPoints

--- arrays/kClosestPoints.java
+++ arrays/kClosestPoints.java
@@ -8,7 +8,7 @@ class Solution{
             dists[i] = dist(points[i]);
         
         Arrays.sort(dists);
-        int distK = dists[K];
+        int distK = dists[K-1];
         int t = 0;
         int[][] ans = new int[K][2];
         for(int i = 0; i < N; i++){
@@ -19,5 +19,12 @@ class Solution{
     }
     public int dist(int[] point){
         return point[0] * point[0] + point[1] * point[1];
+    }
+}
+
+class Solution{
+    public int[][] kClosest(int[][] points){
+        Arrays.sort(points, (p1, p2) -> p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] + p2[1] * p2[1]);
+        return Arrays.copyOfRange(points, 0, K);
     }
 }

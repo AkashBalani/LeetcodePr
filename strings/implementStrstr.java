00c41cfa15767392f3ee1889e5617bf2893d5046
AkashBalani <balani.a@husky.neu.edu>
2020-06-03 22:58:33 -0400

    Added another solution for strStr

--- strings/implementStrstr.java
+++ strings/implementStrstr.java
@@ -8,3 +8,24 @@ class Solution{
         return -1;
     }
 }
+
+class Solution{
+    public int strStr(String haystack, String needle){
+        int L = needle.length(), N = haystack.length();
+        int pN = 0;
+        while(pN < N - L + 1){
+            while(pN < N - L + 1 && haystack.charAt(pn) != needle.charAt(0)) ++pN;
+
+            int currLen = 0, pL = 0;
+            while(pL < L && pN < N && haystack.charAt(pN) == needle.charAt(pL)){
+                ++pN;
+                ++pL;
+                ++currLen;
+            }
+            if(currLen == L) return pN - L;
+
+            pN = pN - currLen + 1;
+        }
+        return -1;
+    }
+}

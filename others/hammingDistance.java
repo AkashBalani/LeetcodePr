85ccaafff3e8baf6733b52f46702c3e7aa5028d9
AkashBalani <balani.a@husky.neu.edu>
2020-06-10 00:54:39 -0400

    Added another solution for Hamming Distance

--- others/hammingDistance.java
+++ others/hammingDistance.java
@@ -4,3 +4,17 @@ class Solution{
         return Integer.bitCount(xor);
     }
 }
+
+class Solution1{
+    public int hammingDistance(int x, int y){
+        int xor = x ^ y;
+        int dist = 0;
+        while(xor != 0){
+            if(xor % 2 == 1){ // WE can also use xor & 1 == 1
+                dist += 1;
+                xor = xor >> 1;
+            }
+        }
+        return dist;
+    }
+}

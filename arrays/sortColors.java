c0c5f03c758b89de1defaa59d9afcaf0c117eccb
AkashBalani <balani.a@husky.neu.edu>
2020-06-11 15:24:04 -0400

    Added official solution for sortColors

--- arrays/sortColors.java
+++ arrays/sortColors.java
@@ -25,3 +25,23 @@ class Solution {
         }
     }
 }
+
+class Solution1{
+    public void sortColors1(int[] nums){
+        int p0 = 0, curr = 0;
+        int p2 = nums.length - 1;
+        while(curr <= p2){
+            if(nums[curr] == 0){
+                int tmp = nums[p0];
+                nums[p0++] = nums[curr];
+                nums[curr++] = tmp;
+            }
+            else if(nums[curr] == 2){
+                int tmp = nums[curr];
+                nums[curr] = nums[p2--];
+                nums[p2--] = tmp;
+            }
+            else curr++;
+        }
+    }
+}

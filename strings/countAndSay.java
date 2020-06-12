02a2d2892dfad0cad81a963e179ebed35c2b4b22
AkashBalani <balani.a@husky.neu.edu>
2020-06-11 20:12:55 -0400

    Added a regex solution for count and say

--- strings/countAndSay.java
+++ strings/countAndSay.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution {
     public String countAndSay(int n) {
         String s = "1";
@@ -29,4 +32,29 @@ public class Solution {
         sb.append(c);
         return sb.toString();
     }
+}
+
+
+class Solution1 {
+  public String countAndSay(int n) {
+    String currSeq = "1";
+
+    // Pattern to match the repetitive digits
+    String regexPattern = "(.)\\1*";
+    Pattern pattern = Pattern.compile(regexPattern);
+
+    for (int i = 1; i < n; ++i) {
+      Matcher m = pattern.matcher(currSeq);
+      StringBuffer nextSeq = new StringBuffer();
+
+      // each group contains identical and adjacent digits
+      while (m.find()) {
+        nextSeq.append(m.group().length() + String.valueOf(m.group().charAt(0)));
+      }
+      // prepare for the next iteration
+      currSeq = nextSeq.toString();
+    }
+
+    return currSeq;
+  }
 }

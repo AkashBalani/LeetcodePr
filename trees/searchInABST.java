387a9b996b2bb0ca15abc0c9700f6c0b3351b7db
AkashBalani <balani.a@husky.neu.edu>
2020-06-15 13:53:51 -0400

    Added another code for BST

--- trees/searchInABST.java
+++ trees/searchInABST.java
@@ -15,4 +15,12 @@ class Solution1 {
   
       return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
     }
-  }
+}
+
+class Solution2 {
+    public TreeNode searchBST(TreeNode root, int val) {
+      while (root != null && val != root.val)
+        root = val < root.val ? root.left : root.right;
+      return root;
+    }
+}

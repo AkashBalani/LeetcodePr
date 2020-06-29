005c5b075e3b0e860be0059c220d6fe4b86283a3
AkashBalani <balani.a@husky.neu.edu>
2020-06-29 01:39:22 -0400

    Added Iterative Solution for Level Order Traversal

--- trees/levelOrderTraversal.java
+++ trees/levelOrderTraversal.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
+import javax.swing.tree.TreeNode;
+
+import sun.misc.Queue;
+
 public class Solution {
     List<List<Integer>> levels = new ArrayList<List<Integer>>();
 
@@ -19,4 +25,29 @@ public class Solution {
         helper(root, 0);
         return levels;
     } 
+}
+
+class Solution1{
+    public List<List<Integer>> levelOrderTraversal(TreeNode root){
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        if(root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int level = 0;
+        while(!queue.isEmpty()){
+            levels.add(new ArrayList<Integer>());
+
+            int level_length = queue.size();
+            for(int i = 0; i < level_length; i++){
+                TreeNode node = queue.remove();
+
+                levels.get(level).add(node.val);
+
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            level++;
+        }
+    }
 }

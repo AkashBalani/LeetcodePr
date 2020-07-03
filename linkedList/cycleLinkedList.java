7629e50c0f1a92ae5b8347b87f632358bcd4033f
AkashBalani <balani.a@husky.neu.edu>
2020-07-03 17:48:35 -0400

    Added another code Cyclic Linked List

--- linkedList/cycleLinkedList.java
+++ linkedList/cycleLinkedList.java
@@ -11,3 +11,21 @@ public class Solution {
         return false;
     }
 }
+
+public class Solution1{
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}

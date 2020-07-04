1a34c89ced7a533909b604b296fc6e96cf327e35
AkashBalani <balani.a@husky.neu.edu>
2020-07-04 15:20:36 -0400

    Added Inorder Solution to sortedToBST problem

--- trees/sortedArrayToBST.java
+++ trees/sortedArrayToBST.java
@@ -8,7 +8,29 @@ class Solution{
 
         TreeNode root = new TreeNode(nums[p]);
         root.left = helper(left, p - 1);
-        root.right = helper(p + 1, rigjt);
+        root.right = helper(p + 1, right);
+
+        return root;
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums){
+        this.nums = nums;
+        return helper(0, nums.length - 1);
+    }
+}
+
+class Solution1{
+    int[] nums;
+
+    public TreeNode helper(int left, int right){
+        if(left > right) return null;
+
+        int p = (left + right) / 2;
+        if((p % 2) == 1) ++p;
+
+        TreeNode root = new TreeNode(nums[p]);
+        root.left = helper(left, p - 1);
+        root.right = helper(p + 1, right);
 
         return root;
     }

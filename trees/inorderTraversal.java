b913eab007520fb875d94ff07c167c90d7f9e974
AkashBalani <balani.a@husky.neu.edu>
2020-07-07 15:02:40 -0400

    Added code for inorder traversal #2

--- trees/inorderTraversal.java
+++ trees/inorderTraversal.java
@@ -12,3 +12,21 @@ class Solution {
         }
     }
 }
+
+public class Solution1 {
+    public List < Integer > inorderTraversal(TreeNode root) {
+        List < Integer > res = new ArrayList < > ();
+        Stack < TreeNode > stack = new Stack < > ();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
+}

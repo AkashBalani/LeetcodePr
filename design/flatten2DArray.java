9923a20e206ed9fbcb9de3b8831cd2b270f16fd0
AkashBalani <balani.a@husky.neu.edu>
2020-07-11 12:36:52 -0400

    Added code for Flattening 2D Array

---
+++ design/flatten2DArray.java
@@ -0,0 +1,23 @@
+import java.util.NoSuchElementException;
+class Vector2D {
+
+    private List<Integer> nums = new ArrayList<>();
+    private int position = 0;
+    
+    public Vector2D(int[][] v) {
+        for(int[] innerVertex: v)
+            for(int num: innerVertex)
+                nums.add(num);
+    }
+    
+    public int next() {
+        if(!hasNext()) throw new NoSuchElementException();
+        int result = nums.get(position);
+        position++;
+        return result;
+    }
+    
+    public boolean hasNext() {
+        return position < nums.size();
+    }
+}

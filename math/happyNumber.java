3223676c750ef995e2e4096e1dd5450cf205922a
AkashBalani <balani.a@husky.neu.edu>
2020-07-12 12:50:22 -0400

    Added slow/fast happy Number

--- math/happyNumber.java
+++ math/happyNumber.java
@@ -23,3 +23,25 @@ class Solution2{
         return n == 1;
     }
 }
+
+class Solution1{
+    public int getNext(int n){
+        int totalSum = 0;
+        while(n > 0){
+            int d = n % 10;
+            n /= 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappyNumber(int n){
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while(fastRunner != 1 && slowRunner != fastRunner){
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(slowRunner));
+        }
+        return fastRunner == 1;
+    }
+}

72f985c9f7750ec790ab797a7d4d20fe058f0163
AkashBalani <balani.a@husky.neu.edu>
2020-07-16 16:59:03 -0400

    Added code for Odd Even Linked List

---
+++ linkedList/oddEvenList.java
@@ -0,0 +1,13 @@
+class Solution{
+    public ListNode oddEvenList(ListNode head){
+        ListNode odd = head, even = odd.next, evenHead = even;
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}

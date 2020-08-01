fd92c731933357766ff9aa8b9828b3a57fd181b8
AkashBalani <balani.a@husky.neu.edu>
2020-08-01 17:27:21 -0400

    Added PQ code for kth largest element

--- sortingAndSearching/KthLargestElementinanArray.java
+++ sortingAndSearching/KthLargestElementinanArray.java
@@ -1,6 +1,21 @@
+import java.util.PriorityQueue;
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length - k];
+    }
+}
+
+class Solution1{
+    public int findKthLargest(int[] nums, int k){
+        final PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int val : nums){
+            pq.offer(val);
+
+            if(pq.size() > k)
+                pq.poll();
+        }
+        return pq.peek();
     }
 }

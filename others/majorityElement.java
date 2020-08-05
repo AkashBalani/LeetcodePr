1c35b610c0e27f843b25be3fd99d3a4f026a6b89
AkashBalani <balani.a@husky.neu.edu>
2020-08-05 15:48:47 -0400

    Added innovative solution to Majority Element

--- others/majorityElement.java
+++ others/majorityElement.java
@@ -21,3 +21,19 @@ class Solution1 {
         return nums[nums.length/2];
     }
 }
+
+class Solution2 {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}

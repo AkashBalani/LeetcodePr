365d29fd7b819e7a24a2c39ef5c94fd2d94c7e7d
AkashBalani <balani.a@husky.neu.edu>
2020-08-07 14:55:19 -0400

    Added a simpler code for nestPointers

--- trees/nextPointersToEachNode.java
+++ trees/nextPointersToEachNode.java
@@ -61,3 +61,19 @@ class Solution1 {
         return root;
     }
 }
+
+public class Solution2 {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode level_start=root;
+        while(level_start!=null){
+            TreeLinkNode cur=level_start;
+            while(cur!=null){
+                if(cur.left!=null) cur.left.next=cur.right;
+                if(cur.right!=null && cur.next!=null) cur.right.next=cur.next.left;
+                
+                cur=cur.next;
+            }
+            level_start=level_start.left;
+        }
+    }
+}

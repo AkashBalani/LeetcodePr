83259b9cc50f1758f77dc6cdb8e0864b4336572e
AkashBalani <balani.a@husky.neu.edu>
2020-08-09 19:14:15 -0400

    Made some changes to the code

--- sortingAndSearching/findPeakElement.java
+++ sortingAndSearching/findPeakElement.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        for(int i = 1; i < nums.length - 1; i++){
-            if(nums[i] > nums[i+1] && nums[i] > nums[i-1])
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] > nums[i+1])
                 return i;
         }
         return nums.length - 1;

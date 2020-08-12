213ea11c262ae8873f7985a9ca79a09b5d13fcd5
AkashBalani <balani.a@husky.neu.edu>
2020-08-12 17:28:26 -0400

    Added an iterative Solution to the problem

--- math/powerOfN.java
+++ math/powerOfN.java
@@ -34,3 +34,22 @@ class Solution1 {
         return fastPow(x, N);
     }
 }
+
+class Solution2 {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        double current_product = x;
+        for (long i = N; i > 0; i /= 2) {
+            if ((i % 2) == 1) {
+                ans = ans * current_product;
+            }
+            current_product = current_product * current_product;
+        }
+        return ans;
+    }
+}

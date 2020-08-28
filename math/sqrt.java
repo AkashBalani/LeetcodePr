ec96b2d1d2727e29ddc9418c7a3b51a7ebb57cd1
AkashBalani <balani.a@husky.neu.edu>
2020-08-28 11:45:09 -0500

    Added code for sqrt with recursion

--- math/sqrt.java
+++ math/sqrt.java
@@ -24,3 +24,13 @@ class Solution1 {
         return right;
     }
 }
+
+class Solution2 {
+    public int mySqrt(int x) {
+      if (x < 2) return x;
+  
+      int left = mySqrt(x >> 2) << 1;
+      int right = left + 1;
+      return (long)right * right > x ? left : right;
+    }
+  }

8796f35da3706fe50a08a15f647c39f4e66b4929
AkashBalani <balani.a@husky.neu.edu>
2020-08-31 13:13:06 -0500

    Added code for inorderSuccessorInBST Iterative

--- trees/inorderSuccessorInBST.java
+++ trees/inorderSuccessorInBST.java
@@ -10,3 +10,17 @@ public TreeNode inorderSuccessor(TreeNode root, TreeNode p){
         return (left != null) ? left : root;
     }
 }
+
+public TreeNode inorderSuccessor2(TreeNode root, TreeNode p){
+    TreeNode succ = null;
+    while(root != null){
+        if(p.val < root.val){
+            succ = root;
+            root = root.left;
+        }
+        else{
+            root = root.right;
+        }
+    }
+    return succ;
+}

b8851ef7bc89f6691992df30e0b090670ac051ab
AkashBalani <balani.a@husky.neu.edu>
2020-09-15 21:25:47 -0500

    Added code for Combination Sum 3 #2

--- backtracking/combinationSum3.java
+++ backtracking/combinationSum3.java
@@ -17,3 +17,33 @@ class Solution {
         }
     }
 }
+
+/*
+    1. k = 3 and n = 7
+    tempList -> [1, 2, 4]
+    list -> [1, 2, 4]
+    public List<List<Integer>> combinationSum3(3, 7)
+        backtrack(list, new ArrayList<Integer>(), 3, 7, 1);
+            if(tempList.size() > k) -> if(0 > 3) -> if(F)
+            else if(tempList.size() == k && remain == 0) -> else if(0 == 3 && remain == 0) -> else if(F)
+            else
+                for(int i = 1; i<=9; i++)
+                    tempList.add(1)
+                    backtrack(list, tempList, k, remain-i, i+1) -> backtrack(list, [1], 3, 7-1, 1+1)
+                    if(tempList.size() > k) -> if(1 > 3) -> if(F)
+                    else if(1 == 3 && remain == 0) -> else if(F)
+                    else
+                        for(int i = 2; i<=9; i++)
+                            tempList.add(2)
+                            backtrack(list, tempList, k, remain-i, i+1) -> backtrack(list, tempList, 3, 6-2, 2+1)
+                                if(tempList.size() > k) -> if(2 > 3) -> if(F)
+                                else if(tempList.size() == k && remain == 0) -> else if(2 == 3 && ...) -> else if(F)
+                                else
+                                    for(int i = 4; i <= 9; i++)
+                                        tempList.add(4)
+                                        backtrack(list, tempList, k, remain-i, i+1) -> backtrack(list, tempList, 3, 4-4, 4+1)
+                                            if(tempList.size() > k) -> if(3 > 3) -> if(F)
+                                            else if(tempList.size() == k && remain == 0) -> else if(3 == 3 && 0 == 0) -> else if(T)
+                                                list.add(new ArrayList<>(tempList))
+                                        tempList.remove(tempList.size() - 1)
+*/

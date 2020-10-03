0a596a0e38f5576aba616c0e57fdd7d977c794da
AkashBalani <balani.a@husky.neu.edu>
2020-10-03 02:15:03 -0500

    Added code for coin Change

--- dynamicProgramming/coinChange.java
+++ dynamicProgramming/coinChange.java
@@ -18,3 +18,17 @@ private int helper(int[] coins, int rem, int[] count) { // rem: remaining coins 
     return count[rem-1];
     }
 }
+
+public class Solution1{
+    public int coinChange(int amount, int[] coins){
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < coins.length; j++){
+                if(coins[j] <= i)
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}

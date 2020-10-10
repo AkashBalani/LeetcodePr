dc0fc6107020a224bf4bfee192742045abb339a3
AkashBalani <balani.a@husky.neu.edu>
2020-10-09 19:10:24 -0500

    Added code for searching in 2D matrix #4

--- sortingAndSearching/search2DMatrix2.java
+++ sortingAndSearching/search2DMatrix2.java
@@ -103,3 +103,23 @@ class Solution2 {
         return searchRec(0, 0, matrix[0].length-1, matrix.length-1);
     }
 }
+
+class Solution3 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // start our "pointer" in the bottom-left
+        int row = matrix.length-1;
+        int col = 0;
+
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] > target) {
+                row--;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else { // found it
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

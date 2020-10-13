f1388dbe5d5c1b80726f4d248eca8a51bd5ee18c
AkashBalani <balani.a@husky.neu.edu>
2020-10-13 16:47:51 -0500

    Added another code for productOfArrayExceptSelf #2

--- arrays/productOfArrayExceptSelf.java
+++ arrays/productOfArrayExceptSelf.java
@@ -49,6 +49,7 @@ class Solution {
     }
 }
 
+
 class Solution1 {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
@@ -61,6 +62,28 @@ class Solution1 {
         for (int i = n - 1; i >= 0; i--) {
             res[i] *= right;
             right *= nums[i];
+        }
+        return res;
+    }
+}
+
+public class Solution2 {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        // Calculate lefts and store in res.
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            if (i > 0)
+                left = left * nums[i - 1];
+            res[i] = left;
+        }
+        // Calculate rights and the product from the end of the array.
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1)
+                right = right * nums[i + 1];
+            res[i] *= right;
         }
         return res;
     }

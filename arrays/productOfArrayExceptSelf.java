953d4302fed0ccffb1baae12b1b0b6833c436073
AkashBalani <balani.a@husky.neu.edu>
2020-10-13 16:30:26 -0500

    Added another code for productOfArrayExceptSelf

--- arrays/productOfArrayExceptSelf.java
+++ arrays/productOfArrayExceptSelf.java
@@ -1,6 +1,6 @@
 // Accepted Solution
 
-class Solution8 {
+class Solution {
     public int[] productExceptSelf(int[] nums) {
 
         // The length of the input array
@@ -46,5 +46,22 @@ class Solution8 {
         }
 
         return answer;
+    }
+}
+
+class Solution1 {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
     }
 }

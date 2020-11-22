30ac80d1aeb6fcb23a98cce003e03d1d3b4744cc
AkashBalani <balani.a@husky.neu.edu>
2020-11-22 11:46:16 -0500

    FindAllNumbersDisappearedinanArray.java

--- arrays/FindAllNumbersDisappearedinanArray.java
+++ arrays/FindAllNumbersDisappearedinanArray.java
@@ -18,3 +18,22 @@ class Solution {
         return output;
     }
 }
+
+// This Solution is O(n) in space
+class Solution{
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i : nums) {
+            int index = Math.abs(i);
+            if (nums[index - 1] > 0) {
+                nums[index - 1] *= -1;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+}

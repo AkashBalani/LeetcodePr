322ff09f1e2e74a8c0052fbb688ae9bd75a38143
AkashBalani <balani.a@husky.neu.edu>
2020-11-29 11:52:39 -0500

    added code for minimumDepthOfABinaryTree

--- trees/minimumDepthOfABinaryTree.java
+++ trees/minimumDepthOfABinaryTree.java
@@ -6,3 +6,13 @@ class Solution {
         return Math.min(1 + minDepth(root.left), 1 + minDepth(root.right));
     }
 }
+
+// Accepted Solution
+class Solution {
+    public static int minDepth(TreeNode root) {
+	    if (root == null)	return 0;
+	    if (root.left == null)	return minDepth(root.right) + 1;
+	    if (root.right == null) return minDepth(root.left) + 1;
+	    return Math.min(minDepth(root.left),minDepth(root.right)) + 1;
+    }
+}

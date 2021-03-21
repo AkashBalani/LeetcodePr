e7b5e043a0b10ce63aa16149fc255f80fdc154e7
AkashBalani <balani.a@husky.neu.edu>
2021-03-21 17:35:43 -0400

    Making edits for reorderedPowerOf2 #2

--- arrays/reorderedPowerOf2.java
+++ arrays/reorderedPowerOf2.java
@@ -1,3 +1,5 @@
 // Notes
 // 1. As the input can be shuffled, we'll sort them in their ascending order O(nlogn)
 // 2. Secondly you can realize the fact that there is a repeating pattern for number of digits incresing 4 (1, 2, 4, 8) , 3(16, 32, 64), 3(128, 258, 512)
+// 3. The input is constrained by (10e9) so we know by this and previous point that power is being checked till (10e29)
+

9a07f4719975e2b9d82d427df4c431bf4a55f8b6
AkashBalani <balani.a@husky.neu.edu>
2021-03-29 19:59:11 -0400

    Added file for targetSum.java #2

--- dynamicProgramming/targetSum.java
+++ dynamicProgramming/targetSum.java
@@ -1,6 +1,5 @@
 // Recursive code
 // O(2^n)
-
 public class Main {
     int count = 0;
     public static void main(String[] args) {
@@ -23,4 +22,31 @@ public class Main {
           calculate(nums, i+1, sum-nums[i], S);
         }
     }
-  }
+}
+
+// This Solution just adds Memoization
+public class Main1 {
+    int count = 0;
+    public int findTargetSumWays(int[] nums, int S) {
+        int[][] memo = new int[nums.length][2001];
+        for (int[] row: memo)
+            Arrays.fill(row, Integer.MIN_VALUE);
+        return calculate(nums, 0, 0, S, memo);
+    }
+    public int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
+        if (i == nums.length) {
+            if (sum == S)
+                return 1;
+            else
+                return 0;
+        } else {
+            if (memo[i][sum + 1000] != Integer.MIN_VALUE) {
+                return memo[i][sum + 1000];
+            }
+            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+            int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
+            memo[i][sum + 1000] = add + subtract;
+            return memo[i][sum + 1000];
+        }
+    }
+}

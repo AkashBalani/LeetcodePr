f8a813584f29d173d5aaaeca797389f95b607ca4
AkashBalani <balani.a@husky.neu.edu>
2021-04-14 17:41:41 -0400

    Added code for containerWithMostWater #2

--- arrays/containerWithMostWater.java
+++ arrays/containerWithMostWater.java
@@ -1,4 +1,18 @@
-class Solution {
+class Solution{
+    public int maxArea(int[] height) {
+        int area = 0, maxArea = 0;
+        for(int i = 0; i < height.length - 1; i++){
+            for(int j = i + 1; j < height.length; j++){
+                area = Math.min(height[i], height[j]) * (j - i);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
+}
+// O(n^2)
+
+class Solution1 {
     public int maxArea(int[] height) {
         int maxArea = 0, l = 0, r = height.length - 1;
         while(l < r){
